117a6bbba64e8002489fc5f4f71c47cc8149fa3c
Eugene Burmako <eburmako@twitter.com>
2017-08-03 16:57:27 -0700

    minimalistic skeleton

--- app/src/main/scala/Test.scala
+++ app/src/main/scala/Test.scala
@@ -1,26 +1,10 @@
 import scala.meta._
 
 object Test {
-  def findUsesofMethodXofClassY(dbnames: Map[Position,Symbol],methodX: String, classY: String): Unit = {
-    println("Method name = " + methodX + " Class name = "+ classY)
-
-    for ((k,v) <- dbnames) {
-      if (s"$v" contains classY + "#" + methodX) {
-        print(s"found use of Method $methodX of Class $classY at position ")
-        println(k.start.offset,k.end.offset)
-      }
-    }
-  }
   def main(args: Array[String]): Unit = {
-    if (args.length != 3) {
-      println("usage: scala Test <classpath> <methodName> <className>")
-      sys.exit(1)
-    }
-
+    if (args.length != 1) sys.error("usage: sbt 'app/run <classpath>'")
     val classpath = Classpath(args(0))
-    println(classpath)
-    val database = Database.load(classpath, Sourcepath(args(0)))
-    val names = database.names
-    findUsesofMethodXofClassY(names,args(1), args(2))
+    val database = Database.load(classpath)
+    println(database)
   }
 }
--- build.sbt
+++ build.sbt
@@ -1,5 +1,4 @@
 lazy val app = project.in(file("app")).settings(
   scalaVersion := "2.11.11",
-  resolvers += Resolver.bintrayRepo("scalameta", "maven"),
-  libraryDependencies += "org.scalameta" %% "scalameta" % "1.9.0-767-33c47b6f"
+  libraryDependencies += "org.scalameta" %% "scalameta" % "2.0.0-M1"
 )
